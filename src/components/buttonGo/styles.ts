29a411330233e9e258eafa1e1a0c553db49c4f36
abigail.lima <macstudio@local>
2023-10-02 09:43:07 -0300

    criando tela de cadasto

--- src/components/buttonGo/styles.ts
+++ src/components/buttonGo/styles.ts
@@ -1,11 +1,41 @@
+import { EButtonType } from 'domains/enums/EButtons';
 import styled from 'styled-components';
 import themes from 'themes/index';
 
-export const Container = styled.div`
+interface IContainerProps {
+  type: EButtonType;
+}
+
+export const Container = styled.div<IContainerProps>`
   display: flex;
   flex-direction: row;
-  background-color: ${themes.colors.v3};
-  border: 2px solid rgba(6, 106, 149, 0.55);
+  background-color: ${(p: IContainerProps) => {
+    if (p.type == EButtonType.default) {
+      return themes.colors.btDefaultBg;
+    } else if (p.type == EButtonType.remove) {
+      return themes.colors.btRemoveBg;
+    } else if (p.type == EButtonType.save) {
+      return themes.colors.btSaveBg;
+    } else if (p.type == EButtonType.cancel) {
+      return themes.colors.btCancelBg;
+    }
+    return themes.colors.btDefaultBg;
+  }};
+
+  border: 2px solid
+    ${(p: IContainerProps) => {
+      if (p.type == EButtonType.default) {
+        return themes.colors.btDefaultBorder;
+      } else if (p.type == EButtonType.remove) {
+        return themes.colors.btRemoveBorder;
+      } else if (p.type == EButtonType.save) {
+        return themes.colors.btSaveBorder;
+      } else if (p.type == EButtonType.cancel) {
+        return themes.colors.btCancelBorder;
+      }
+      return themes.colors.btDefaultBorder;
+    }};
+
   border-radius: 15px;
   height: 47px;
   justify-content: center;
@@ -13,17 +43,42 @@ export const Container = styled.div`
   cursor: pointer;
 
   &:hover {
-    background-color: #fff;
+    background-color: ${(p: IContainerProps) => {
+      if (p.type == EButtonType.default) {
+        return themes.colors.btDefaultBgHover;
+      } else if (p.type == EButtonType.remove) {
+        return themes.colors.btRemoveBgHover;
+      } else if (p.type == EButtonType.save) {
+        return themes.colors.btSaveBgHover;
+      } else if (p.type == EButtonType.cancel) {
+        return themes.colors.btCancelBgHover;
+      }
+      return themes.colors.btDefaultBgHover;
+    }};
   }
 
   &:active {
-    background-color: #949494;
+    background-color: ${(p: IContainerProps) => {
+      if (p.type == EButtonType.default) {
+        return themes.colors.btDefaultBgActive;
+      } else if (p.type == EButtonType.remove) {
+        return themes.colors.btRemoveBgActive;
+      } else if (p.type == EButtonType.save) {
+        return themes.colors.btSaveBgActive;
+      } else if (p.type == EButtonType.cancel) {
+        return themes.colors.btCancelBgActive;
+      }
+      return themes.colors.btDefaultBgActive;
+    }};
   }
 `;
 
-export const Titulo = styled.div`
+interface ITitleProps {
+  type: EButtonType;
+}
+
+export const Title = styled.div<ITitleProps>`
   display: flex;
-  color: ${themes.colors.v2};
   text-align: center;
   font-size: 18px;
   font-family: ${themes.fontFamily.inter};
@@ -33,4 +88,17 @@ export const Titulo = styled.div`
   flex-wrap: wrap;
   line-height: 35px;
   padding: 0 30px;
+
+  color: ${(p: IContainerProps) => {
+    if (p.type == EButtonType.default) {
+      return themes.colors.btDefaultColor;
+    } else if (p.type == EButtonType.remove) {
+      return themes.colors.btRemoveColor;
+    } else if (p.type == EButtonType.save) {
+      return themes.colors.btSaveColor;
+    } else if (p.type == EButtonType.cancel) {
+      return themes.colors.btCancelColor;
+    }
+    return themes.colors.btDefaultColor;
+  }};
 `;
--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -6,6 +6,30 @@ const colors = {
   v4: '#949494',
   v5: '#92d5f1',
   v6: '#4795b6',
+
+  btDefaultBg: '#C0ECFF',
+  btDefaultBgHover: '#8bbed4',
+  btDefaultBgActive: '#719fb3',
+  btDefaultBorder: '#DAEFF9',
+  btDefaultColor: '#0876A6',
+
+  btRemoveBg: '#B93E3E',
+  btRemoveBgHover: '#d84e4e',
+  btRemoveBgActive: '#953131',
+  btRemoveBorder: '#FF8E8E',
+  btRemoveColor: '#FFFFFF',
+
+  btSaveBg: '#DAEFF9',
+  btSaveBgHover: '#ecf5f9',
+  btSaveBgActive: '#b1c1c9',
+  btSaveBorder: '#DAEFF9',
+  btSaveColor: '#0876A6',
+
+  btCancelBg: '#0882B7',
+  btCancelBgHover: '#159eda',
+  btCancelBgActive: '#0882B7',
+  btCancelBorder: '#66ADCC',
+  btCancelColor: '#FFFFFF',
 };
 
 export default colors;
