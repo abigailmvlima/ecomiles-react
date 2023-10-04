90168ad6628f45dff674da95d71652bcf770630b
abigail.lima <macstudio@local>
2023-10-04 10:36:21 -0300

    ajust

--- api/app.js
+++ api/app.js
@@ -1,17 +1,16 @@
 require("dotenv").config();
-var createError = require("http-errors");
-var express = require("express");
+
+const express = require("express");
 const session = require("express-session");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-// var logger = require('morgan');
+const cookieParser = require("cookie-parser");
+
+const createError = require("http-errors");
+const path = require("path");
 const bodyParser = require("body-parser");
-const { azureConfig } = require("./config");
 
 const {
   getAuthToken,
   postPatient,
-  printPatientInfo,
   getPatients,
   deletePatients,
   updatePatients,
@@ -21,18 +20,17 @@ var app = express();
 
 app.use(
   session({
-    secret: "secretteste",
+    secret: "secrettest",
     saveUninitialized: true,
     resave: true,
     token: {},
     logado: {
-      apelido: "elio.lima",
-      nome: "Elio Gonçalves de Lima",
-      email: "diretoria@maxtime.info",
-      endfoto: "6618004.jpg",
+      apelido: "usuario.sobrenome",
+      nome: "Nome do Usuario",
+      email: "usuario@dominio.com",
     },
     sis: {
-      version: "0.0.1",
+      version: "1.0.0",
     },
   })
 );
@@ -40,23 +38,19 @@ app.use(
 app.set("views", path.join(__dirname, "/routes/pages/views"));
 app.set("view engine", "ejs");
 
-// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(express.json());
 
 app.get("/patients", async (req, res) => {
   const accessToken = await getAuthToken();
   const data = await getPatients(accessToken);
-
   res.json(data?.entry || []);
 });
 
 app.post("/patients", async (req, res) => {
-  console.log(123);
   const accessToken = await getAuthToken();
   const patientId = await postPatient(accessToken, req.body);
   res.json({ patientId });
@@ -64,7 +58,6 @@ app.post("/patients", async (req, res) => {
 
 app.delete("/patients/:id", async (req, res) => {
   const patientId = req.params.id;
-  console.log(999, patientId);
   const accessToken = await getAuthToken();
   const data = await deletePatients(patientId, accessToken);
   res.json(data);
@@ -77,18 +70,14 @@ app.put("/patients/:id", async (req, res) => {
   res.json(data);
 });
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   if (err.status == 404) {
     res.redirect("/404");
     return false;
