1216b899c112f2024bd27777b2d78f16b609985f
Abigail Lima <abigailmvlima@outlool.com>
2023-10-13 14:15:52 -0300

    inclusao imagens no index

--- client/src/assets/svg/index.ts
+++ client/src/assets/svg/index.ts
@@ -1,16 +1,19 @@
-import { ReactComponent as BSProfissional } from './bSProfissional.svg';
 import { ReactComponent as LogoHeader } from './biaSilva.svg';
+import { ReactComponent as BSProfissional } from './bsProfissional.svg';
 import { ReactComponent as ChevronRight } from './chevronRight.svg';
 import { ReactComponent as Facebook } from './facebook.svg';
 import { ReactComponent as Hello } from './hello.svg';
 import { ReactComponent as IconEdit } from './iconEdit.svg';
 import { ReactComponent as Instagram } from './instagram.svg';
+import { ReactComponent as Mail } from './mail.svg';
 import { ReactComponent as MeAbout } from './meAbout.svg';
 import { ReactComponent as MyExpiriences } from './myExpiriences.svg';
 import { ReactComponent as MyHobbies } from './myHobbies.svg';
 import { ReactComponent as MyTrainings } from './myTrainings.svg';
+import { ReactComponent as Phone } from './phone.svg';
 import { ReactComponent as PhotoBS } from './photoBS.svg';
 import { ReactComponent as TitleFullStack } from './titleFullStack.svg';
+import { ReactComponent as UpDate } from './upDate.svg';
 import { ReactComponent as YouTube } from './youtube.svg';
 
 const svg = {
@@ -28,6 +31,9 @@ const svg = {
   MyTrainings,
   MyExpiriences,
   MyHobbies,
+  Mail,
+  Phone,
+  UpDate,
 };
 
 export default svg;
